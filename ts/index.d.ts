7f69b4a2637544cb791479b0ac13b692d4d7bc2d
cuichuanteng <cuichuanteng@pwrd.com>
2022-03-28 11:04:18 +0800

    reference add ngx_http_js_module and ngx_stream_js_module

--- ts/index.d.ts
+++ ts/index.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="njs_modules/crypto.d.ts" />
 /// <reference path="njs_modules/fs.d.ts" />
 /// <reference path="njs_modules/querystring.d.ts" />
+/// <reference path="ngx_http_js_module.d.ts" />
+/// <reference path="ngx_stream_js_module.d.ts" />
